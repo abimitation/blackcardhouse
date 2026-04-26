e8fb5e2e89b3e050eff4b9dabd5d24bd48c357bb
Ambex Developer <developer.7n5x@ambex.lv>
2026-04-26 18:34:44 +0300

    default language turkish

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,7 +2,7 @@ import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 
 // Can be imported from a shared config
-const locales = ['en', 'tr'];
+const locales = ['tr', 'en'];
 
 export default getRequestConfig(async ({ requestLocale }) => {
     const locale = await requestLocale;
