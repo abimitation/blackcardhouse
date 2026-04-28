65c60ea32643135e720859c9eb8f9f8e71da441e
Ambex Developer <developer.7n5x@ambex.lv>
2026-04-28 19:59:21 +0300

    fixing error

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,11 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { routing } from './routing';
+import { routing, Locale } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
     const locale = await requestLocale;
     // Validate that the incoming `locale` parameter is valid
-    if (!routing.locales.includes(locale as string)) notFound();
+    if (!routing.locales.includes(locale as Locale)) notFound();
 
     return {
         locale: locale as string,
